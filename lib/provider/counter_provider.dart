8e94b485fe439d9e92a23776d0c32a4c2e5e3b00
myworksbt <myjobsalert20@gmail.com>
2025-04-28 01:42:39 +0530

    Initial commit

---
+++ lib/provider/counter_provider.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class CounterProvider with ChangeNotifier {
+  int _count = 0;
+
+  int get getCount  => _count;
+
+  void increment () {
+    _count ++;
+    notifyListeners();
+  }
+
+  void decrement () {
+    _count --;
+    notifyListeners();
+  }
+
+}
